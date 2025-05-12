2e41b70f8607ed7470f6e09c19200c80145ad353
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2025-05-12 14:39:32 +0200

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt update
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
+apt update
+apt install docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl status docker
