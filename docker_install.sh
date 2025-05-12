5739e1f83047eeb5bf4f6fe9c24dd5f5f383554a
Pascal Sené <38534891+darkw0lf@users.noreply.github.com>
2025-05-12 14:46:15 +0200

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 apt update
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 apt update
-apt install docker-ce docker-ce-cli containerd.io
+apt install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
 systemctl status docker
